049aa1ebafd4e821174022c534b7f87d7f2c3dac
Pablo Miceli <d1urno@gmx.com>
2020-09-09 16:40:12 -0300

    refactor: format code

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
 </template>
 
 <script lang="ts">
@@ -10,7 +12,10 @@ import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: {
+      type: String,
+      required: true
+    }
   },
   setup() {
     const count = ref(0)
