1ad6da1c19299521d5d472d8d994838635621ca0
Pablo Miceli <d1urno@gmx.com>
2020-09-23 12:42:33 -0300

    chore: prepare Apollo client configurations
    - install @vue/apollo-composable
    
    - install apollo-cache-inmemory
    
    - install apollo-client
    
    - install apollo-link-context
    
    - install apollo-link-http
    
    - install graphql
    
    - install graphql-tag

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,19 @@ import { StoreModule } from './modules/store'
 import { HomeModule } from './modules/home'
 import { CoreModule } from './modules/core'
 
-function bootstrap() {
-  const routerModule = new RouterModule()
-  routerModule.install()
+// Vite dot env TS declaration
+declare global {
+  interface ImportMeta {
+    env: any
+  }
+}
 
+function bootstrap() {
   const storeModule = new StoreModule()
   storeModule.install()
+
+  const routerModule = new RouterModule()
+  routerModule.install()
 
   const homeModule = new HomeModule(routerModule.router!, storeModule.store!)
   homeModule.install()
