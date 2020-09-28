8f14d0796747f7883200be033f9c7568f6b07372
Pablo Miceli <d1urno@gmx.com>
2020-09-28 20:52:07 -0300

    feat:(home) add dummy about page

--- src/modules/home/router/home-routes.ts
+++ src/modules/home/router/home-routes.ts
@@ -6,5 +6,10 @@ export const homeRoutes: RouteRecordRaw[] = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/About.vue')
   }
 ]
---
+++ src/modules/home/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab at, cumque
+    deserunt incidunt, ipsum laboriosam magni odio qui quia rerum sint tempore
+    temporibus? Eius modi necessitatibus nostrum quibusdam, velit voluptates?
+  </p>
+</template>
