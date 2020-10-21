e8a4605a750c8bf7a212a4abccd3df2b8ffafbc0
anakais <v8.alexandre@gmail.com>
2020-10-21 18:17:08 -0300

    Header complete

--- src/modules/home/views/Home.vue
+++ src/modules/home/views/Home.vue
@@ -1,4 +1,74 @@
 <template>
+  <header>
+    <section class="relative">
+      <picture>
+        <!-- webp -->
+        <source
+          srcset="../assets/header-img-sm.webp"
+          media="(max-width: 767px)"
+          type="image/webp"
+        />
+        <source
+          srcset="../assets/header-img.webp"
+          media="(min-width: 768px)"
+          type="image/webp"
+        />
+        <!-- png -->
+        <source
+          srcset="../assets/header-img-sm.png"
+          media="(max-width: 767px)"
+          type="image/png"
+        />
+        <source
+          srcset="../assets/header-img.png"
+          media="(min-width: 768px)"
+          type="image/png"
+        />
+        <img alt="Header universe image" src="../assets/header-img.png" />
+      </picture>
+
+      <div class="absolute top-0 w-full h-full">
+        <section
+          class="container flex flex-col justify-center h-full pb-12 mx-auto
+          sm:items-center sm:flex-row-reverse
+          md:pb-20
+          lg:max-w-screen-lg lg:pb-32"
+        >
+          <img
+            class="w-40 mx-auto mt-6 sm:w-3/12 sm:mt-0 lg:w-1/3"
+            src="../assets/header-logo.svg"
+            alt="BlueFields Logo"
+          />
+          <div class="px-6 text-white sm:w-9/12 sm:px-12 lg:w-2/3">
+            <h1
+              class="text-3xl leading-snug text-center sm:text-left text-shadow sm:text-4xl"
+            >
+              Live the teachings <br />
+              Be a volunteer
+            </h1>
+            <div
+              class="hidden w-16 h-1 mx-auto my-6 bg-white sm:block sm:ml-0"
+            />
+            <p
+              class="hidden text-base sm:block text-shadow lg:pr-16 lg:text-xl"
+            >
+              Blue Fields has been created to unite the readers of The Urantia
+              Book, in Latin America, who desire to volunteer, in an intelligent
+              ministry of the authentic needs of our neighbor, with altruistic
+              projects.
+            </p>
+          </div>
+        </section>
+      </div>
+    </section>
+    <p class="px-6 md:w-2/3 sm:hidden">
+      Aqui temos um texto descritivo sobre o que é a página... Lorem ipsum dolor
+      sit amet, consectetur adipiscing elit. Vivamus dictum fermentum ligula
+      gravida feugiat. Fusce molestie at nibh id varius. Nunc suscipit hendrerit
+      ex, vel commodo arcu dignissim porttitor. Ut vestibulum dui eu ligula
+      vehicula, non rhoncus tortor bibendum. Nullam scelerisque metus placerat
+    </p>
+  </header>
   <p v-for="project in projects" :key="project._id" class="bg-green-500">
     {{ project }}
   </p>
@@ -44,3 +114,13 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+h1 {
+  @apply font-bold font-serif2;
+}
+
+.text-shadow {
+  text-shadow: 0 4px 8px rgba(0, 0, 0, 0.4), 0 2px 4px rgba(0, 0, 0, 0.4);
+}
+</style>
